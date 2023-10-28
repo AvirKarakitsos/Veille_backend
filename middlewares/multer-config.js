6cb27675a13bb05b7062d1640cbb92c03bef6d74
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-10-28 22:48:53 +0200

    refactor: comments multer-config

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,10 +1,10 @@
 const multer = require('multer')
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-}
+// const MIME_TYPES = {
+//   'image/jpg': 'jpg',
+//   'image/jpeg': 'jpg',
+//   'image/png': 'png'
+// }
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_')
-    const extension = MIME_TYPES[file.mimetype]
+    //const extension = MIME_TYPES[file.mimetype]
     callback(null, Date.now() + name)
   }
 })
